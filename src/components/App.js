00f4096f263c768b2eeb4b252d6996fd1435441c
shyam <shyam@gmail.com>
2021-07-17 15:24:20 +0530

    install codemirror and test it's operation

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import {UnControlled as CodeMirror} from 'react-codemirror2';
 
 function App() {
   return (
     <div className="App">
         hello
+        <CodeMirror
+          value= "<h1> React-codemirror2 </h1>"
+          options={{
+            mode: 'xml',
+            theme: 'material',
+            lineNumbers:true
+          }}
+          onChange= {(editor,data,value)=>{
+            
+          }}
+          />
     </div>
   );
 }
